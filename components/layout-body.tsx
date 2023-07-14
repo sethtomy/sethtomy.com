c00c0c84bff738fbe98354d8d7aba019e2b1f228
sethtomy <sethtomy@gmail.com>
2023-07-13 22:08:05 -0400

    Basic Sidebar functionality.

---
+++ components/layout-body.tsx
@@ -0,0 +1,54 @@
+import styles from "./layout.module.css";
+import Image from "next/image";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import LayoutHeader from "./layout-header";
+
+const name = 'Seth Tomy';
+
+export default function LayoutBody({ home, children }) {
+  return (
+    <div>
+      <LayoutHeader />
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt=""
+            />
+            <h1 className={utilStyles.heading2x1}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <Image
+                priority
+                src="/images/profile.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt=""
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
+}
